8e568215ec6e57180a8e5209f98abe71f17f534a
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-07-23 21:42:40 +0900

    [#1] Feedback assignment (2)

--- src/server.js
+++ src/server.js
@@ -1,23 +1,10 @@
 import express from "express";
 
 const app = express();
-const PORT = 4000;
 
-const handleHome = (res, req) => res.render("home");
+app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+app.get("/about", (req, res) => res.send("<h1>About</h1>"));
+app.get("/contact", (req, res) => res.send("<h1>Contact</h1>"));
+app.get("/login", (req, res) => res.send("<h1>Login</h1>"));
 
-const handleAbout = (res, req) => res.render("about");
-
-const handleContact = (res, req) => res.render("contact");
-
-const handleLogin = (res, req) => res.render("login");
-
-app.get("/", handleHome);
-app.get("/about", handleAbout);
-app.get("/contact", handleContact);
-app.get("/login", handleLogin);
-
-const handleListening = () => console.log("Listening now!");
-
-app.listen(PORT, handleListening);
-
-//서버 실행 어떻게 하지? Console에서 npm run start 안 먹힘
+app.listen(() => `Listening!✅`);
