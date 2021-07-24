73c4d50d2a7b5fd6a21ae3929c977465d4a8ca03
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-07-24 18:01:47 +0900

    [#2] Add assignment (3)

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 import express from "express";
-//const express = require('express');
 
 const app = express();
 const PORT = 4040;
 
 app.get("/", (req, res) => res.send("<h1>Home</h1>"));
-app.get("/about", (req, res) => res.send("<h1>About</h1>"));
-app.get("/contact", (req, res) => res.send("<h1>Contact</h1>"));
-app.get("/login", (req, res) => res.send("<h1>Login</h1>"));
+app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
 
+// Codesandbox gives us a PORT :)
 app.listen(PORT, () => `Listening!✅`);
