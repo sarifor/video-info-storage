300db05dfe0161963405a482bd6d8da170509fc7
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-07-24 17:33:47 +0900

    [#1] Fix assignment (2)

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+//const express = require('express');
 
 const app = express();
+const PORT = 4040;
 
 app.get("/", (req, res) => res.send("<h1>Home</h1>"));
 app.get("/about", (req, res) => res.send("<h1>About</h1>"));
 app.get("/contact", (req, res) => res.send("<h1>Contact</h1>"));
 app.get("/login", (req, res) => res.send("<h1>Login</h1>"));
 
-app.listen(() => `Listening!✅`);
+app.listen(PORT, () => `Listening!✅`);
