d628e83f078e1ecc18c0cfb9875b398c573a93e0
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-05 07:48:25 +0900

    [#8] Solve assginment (11) where add /movies/:id

--- src/movieController.js
+++ src/movieController.js
@@ -3,8 +3,6 @@ import Movie from "./models/Movie";
 export const seeAllMovies = async (req, res) => {
     // Movie DB의 모든 값을 allMovies 오브젝트에 담아서, see.pug에 보내기
     const allMovies = await Movie.find({});
-    // await Movie.create( { name: "Marina", summary: "ESPA", year: 2011, rating: 5, genres: "Music" } );
-    // console.log(allMovies);
     return res.render("see", { pageTitle: "seeAllMovies", allMovies });
 };
 
@@ -15,8 +13,7 @@ export const getUpload = (req, res) => {
 
 
 export const postUpload = async (req, res) => {
-  const {name, summary, year, rating, genres} = req.body;
-  // console.log(name, summary, year, rating, genres);
+  const { name, summary, year, rating, genres } = req.body;
   await Movie.create({
       name, 
       summary, 
@@ -26,25 +23,28 @@ export const postUpload = async (req, res) => {
   return res.redirect("/");
 }
 
+export const watch = async (req, res) => {
+  // Movie 리스트를 클릭함으로 params에 담아 보낸 아이디를 MOVIE DB에 돌려 movie obj 확보하여, watch.pug로 보내기
+  const { id } = req.params; // _id가 아닌 이유: movieRouter.get("/:id", watch);
+  const amovie = await Movie.findById(id);
+  return res.render("watch", { amovie });
+}
+
 /*
 export const getEdit = (req, res) => {
-  //params에서 얻은 아이디를 MOVIE DB에 돌려 movie obj 확보하여, videoEdit.pug로 보내기
+  // params에서 얻은 아이디를 MOVIE DB에 돌려 movie obj 확보하여, videoEdit.pug로 보내기
 }
 
 export const postEdit = (req, res) => {
-  //params에서 얻은 아이디와 body에서 얻은 새 값을 MOVIE DB에 돌리고, /로 돌아가기
+  // params에서 얻은 아이디와 body에서 얻은 새 값을 MOVIE DB에 돌리고, /로 돌아가기
 
-}
-
-export const watch = (req, res) => {
-  //params에서 얻은 아이디를 MOVIE DB에 돌려 movie obj 확보하여, watch.pug로 보내기
 }
 
 export const deleteVideo = (req, res) => {
-  //params에서 얻은 아이디로 MOVIE DB에서 movie 특정하여 삭제하고, /로 돌아가기
+  // params에서 얻은 아이디로 MOVIE DB에서 movie 특정하여 삭제하고, /로 돌아가기
 }
 
 export const search (req, res) => {
-  //params에서 얻은 검색어를 MOVIE DB에 돌려 검색결과 배열을 확보하여, see.pug 하단에 보여주기
+  // params에서 얻은 검색어를 MOVIE DB에 돌려 검색결과 배열을 확보하여, see.pug 하단에 보여주기
 }
 */
--- src/movieRouter.js
+++ src/movieRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getUpload, postUpload } from "./movieController";
+import { getUpload, postUpload, watch } from "./movieController";
 
 const movieRouter = express.Router();
 
 movieRouter.get("/upload", getUpload);
 movieRouter.post("/upload", postUpload);
-
+movieRouter.get("/:id", watch);
 export default movieRouter;
