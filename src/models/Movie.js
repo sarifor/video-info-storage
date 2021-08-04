f91f2611266d86c436a02f3ef5a78a773523727e
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-04 09:09:55 +0900

    Solve assginment (11) where find() not work

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,38 +1,13 @@
 import mongoose from "mongoose";
 
-/*
-PLEASE ADD YOUR USERNAME IN THIS LINE.
-ALL THE MODELS YOU WILL CREATE WILL HAVE YOUR USERNAME APPENDED TO THEM
-SO YOU CAN SEARCH / ADD / EDIT / DELETE YOUR DOCUMENTS ONLY.
-PLEASE FOLLOW THIS STEP
-WE NEED TO SHARE THE SAME DB SO NICO CAN CHECK OUT EVERYBODYS PROJECT.
-🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
-*/
-const YOUR_USERNAME = "Karina";
-
 const MovieSchema = new mongoose.Schema({
-  title: String,
+  name: String,
   summary: String,
   year: Number,
   rating: Number,
   genres: String,
 });
 
-if (YOUR_USERNAME === null || typeof YOUR_USERNAME !== "string") {
-  /*
-  PLEASE ADD YOUR USERNAME ON THE LINE 10
-  THIS LINE WILL REMIND YOU IF YOU HAVEN'T ADDED IT
-  PLEASE DONT REMOVE THIS LINE
-  */
-  throw Error(
-    "❌  Please add your username in the line 10 of models/Movie.js  ❌"
-  );
-}
+const Movie = mongoose.model("Movie", MovieSchema);
 
-if (YOUR_USERNAME.includes("@")) {
-  throw Error("❌  Please remove the @ from your username  ❌");
-}
-
-// const model = mongoose.model(`Movie_${YOUR_USERNAME}`, MovieSchema);
-const model = mongoose.model("Movie_Karina", MovieSchema);
-export default model;
+export default Movie;
--- src/movieController.js
+++ src/movieController.js
@@ -1,8 +1,9 @@
-import model from "./models/Movie";
+import Movie from "./models/Movie";
 
 export const seeAllMovies = async (req, res) => {
     // Movie DB의 모든 값을 allMovies 오브젝트에 담아서, see.pug에 보내기
-    const allMovies = await model.find({});
-    console.log(allMovies);
-    return res.render("see", { pageTitle: "title dayo", allMovies: allMovies });
+    const allMovies = await Movie.find({});
+    // await Movie.create( { name: "Marina", summary: "ESPA", year: 2011, rating: 5, genres: "Music" } );
+    // console.log(allMovies);
+    return res.render("see", { pageTitle: "title it is", allMovies });
 };
