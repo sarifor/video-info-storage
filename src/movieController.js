7052f3bb9bfe795a6459e60fcc45565566efb28d
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-05 08:37:51 +0900

    [#8] Solve assginment (11) where add link for /movies/:id/edit

--- src/movieController.js
+++ src/movieController.js
@@ -46,18 +46,22 @@ export const postEdit = async (req, res) => {
   const amovie = await Movie.findById(id);
   console.log(amovie);
 
-  // amovie obj에 갱신값 추가하고, Movie.XXX(업뎃하는 함수)로 Movie DB에 업뎃하고, /로 리다이렉트  
+  // amovie obj에 갱신값 추가하고, Movie.XXX로 Movie DB에 업뎃하고, /로 리다이렉트  
   await Movie.findByIdAndUpdate(id, {name, summary, year, rating, genres});
   return res.redirect("/");
 }
 
+/*
+export const search (req, res) => {
+  // params에서 얻은 검색어를 MOVIE DB에 돌려 검색결과 배열을 확보하여, see.pug 하단에 보여주기
+  const { id } = req.params;
+  const amovie = await Movie.findById(id);
+  return res.render("edit", { pageTitle: "getEdit", amovie });  
+}
+*/
 
 /*
 export const deleteVideo = (req, res) => {
   // params에서 얻은 아이디로 MOVIE DB에서 movie 특정하여 삭제하고, /로 돌아가기
-}
-
-export const search (req, res) => {
-  // params에서 얻은 검색어를 MOVIE DB에 돌려 검색결과 배열을 확보하여, see.pug 하단에 보여주기
 }
 */
