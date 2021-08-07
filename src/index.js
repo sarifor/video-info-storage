b6e151bf000847e9ad7205a9158ec144f26cc0f1
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-07 18:55:26 +0900

    [#7] Feedback assignment (10)

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import path from "path";
 import bodyParser from "body-parser";
 
 const app = express();
-const PORT = 4040;
-
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => console.log(`✅  Server Ready!`));
+// Codesanbox does not need PORT :)
+app.listen(() => console.log(`✅  Server Ready!`));
--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
 
 // Create a Movie Model here.
-const movieSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  summary: { type: String, required: true },
-  year: { type: Number, required: true },
-  meta: {
-    rating: { type: Number, required: true }
+const MovieSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
   },
-  genres: [{ type: String, required: true }]
+  summary: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  genres: [String]
 });
-
-const Movie = mongoose.model("Movie", movieSchema);
-export default Movie;
