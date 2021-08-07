f95d14e91f275104be76043ab38262a7fc0f756b
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-07 16:31:47 +0900

    [#6] Solve assignment (9) where not work pug

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import express from "express";
 const app = express();
 const PORT = 4040;
 
-app.set("views", process.cwd() + '/src/views');
-console.log(process.cwd());
-console.log(app.get("views"));
-
+app.set("views", process.cwd() + '/views');
+app.set("views engine", 'pug');
 app.use("/", movieRouter);
 
 app.listen(PORT, () => {
