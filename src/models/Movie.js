d573dabde863bc8d8c02ea985fadc8d8d08d910c
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-07 18:49:28 +0900

    [#7] Solve assignment (10) which only for schema practice

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,3 +1,15 @@
 import mongoose from "mongoose";
 
 // Create a Movie Model here.
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  summary: { type: String, required: true },
+  year: { type: Number, required: true },
+  meta: {
+    rating: { type: Number, required: true }
+  },
+  genres: [{ type: String, required: true }]
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+export default Movie;
