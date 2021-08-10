0a96f5614763e104ddb2585ae838f65f85139db8
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-10 09:32:42 +0900

    [#14] Add assignment (15)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
-import "./db";
-import "./models/User";
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
-import session from "express-session";
-import userRouter from "./userRouter";
-import { localsMiddleware } from "./middlewares";
+import multer from "multer";
+import fs from "fs";
 
 const app = express();
-const PORT = 4040;
-
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true
-  })
-);
-app.use(localsMiddleware);
-app.use("/", userRouter);
 
-app.listen(PORT, () => console.log(`✅  Server Ready!`));
+// Codesanbox does not need PORT :)
+app.listen(4000, () => console.log(`Listening!`));
--- src/middlewares.js
+++
@@ -1,6 +0,0 @@
-export const localsMiddleware = (req, res, next) => {
-  res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.siteTitle = "Como Users";
-  res.locals.loggedInUser = req.session.user;
-  next();
-};
