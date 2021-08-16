a57c4325a9ca40bba7903a6fdc5bc709e89ba706
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-16 17:05:04 +0900

    [#17] Solve Assignment (18)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ const videoController = document.getElementById("videoController");
 const psBtn = videoController.querySelector("#playPauseBtn");
 const volumeBtn = videoController.querySelector("#volume");
 const volumeRange = videoController.querySelector("#volumeRange");
+const time = videoController.querySelector("#time");
+const timeline = videoController.querySelector("#timeline");
+const fs = videoController.querySelector("#fullScreen");
+
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
+
+const updateTime = () => {
+  time.innerHTML = video.currentTime;
+};
+
+const handleLoadedMetadata = () => {
+  timeline.max = video.duration;
+  console.log(timeline.max);
+};
 
 const handlePlayAndStop = () => {
   if (video.paused) {
@@ -45,6 +58,45 @@ const handleVolume = (event) => {
   video.volume = volumeValue = value;
 };
 
+const handleTimeChange = (event) => {
+  const { target: { value } } = event;
+  video.currentTime = value;
+};
+
+const handleFullScreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+  } else {
+    videoController.requestFullscreen();
+  }
+};
+
+const handleKeyDown = (event) => {
+  const { key } = event;
+  if (key === "f") {
+    handleFullScreen();
+  } else if (key === " ") {
+    handlePlayAndStop();
+  } else if (key === "Escape") {
+    const fullscreen = document.fullscreenElement;
+    if (fullscreen === null) {
+      alert("Please use ESC only when the video is fullscreen :)");
+    } else {
+      document.exitFullscreen();
+    };
+  };
+
+};
+
+video.addEventListener("loadeddata", handleLoadedMetadata);
+video.ontimeupdate = () => {
+  updateTime();
+};
+video.addEventListener("ontimeupdate", updateTime);
+timeline.addEventListener("input", handleTimeChange);
 psBtn.addEventListener("click", handlePlayAndStop);
 volumeBtn.addEventListener("click", handleSound);
 volumeRange.addEventListener("input", handleVolume);
+fs.addEventListener("click", handleFullScreen);
+window.addEventListener("keydown", handleKeyDown);
