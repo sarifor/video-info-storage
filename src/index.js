5284cf149e45637e7cdeac5b01f791109a77b3cb
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-20 11:00:44 +0900

    [#18] Feedback Assignment (19)

--- src/index.js
+++ src/index.js
@@ -1,49 +1,117 @@
-const startBtn = document.getElementById("startBtn");
-const audio = document.getElementById("preview");
+const { async } = require("regenerator-runtime");
+
+const recordBtn = document.getElementById("recordBtn");
+const audio = document.querySelector("audio");
+
+const playPauseBtn = document.getElementById("playPauseBtn");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volumeRange");
 
 let stream;
 let recorder;
-let audioFile;
+let recordAudio;
+let volumeValue = 0.5;
+audio.volume = volumeValue;
 
-const startDownload = () => {
-    const a = document.createElement("a");
-    a.href = audioFile;
-    a.download = "Audio.mp4";
-    document.body.appendChild(a);
-    a.click();
+const handleDownload = () => {
+  recordBtn.removeEventListener("click", handleDownload);
+  recordBtn.innerText = "Start Recording";
+  const a = document.createElement("a");
+  a.href = recordAudio;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+  recordBtn.addEventListener("click", handleStart);
 };
 
-const stopRecording = () => {
-    startBtn.innerText = "Download Recording";
-    startBtn.removeEventListener("click", stopRecording);
-    startBtn.addEventListener("click", startDownload);
-    
+const handleStart = async () => {
+  recordBtn.innerText = "Download Recording";
+  playPauseBtn.className = "fas fa-play";
+  playPauseBtn.style.color = "#8b8b8b";
+  recordBtn.disabled = true;
+  recordBtn.removeEventListener("click", handleStart);
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false
+  });
+  audio.srcObject = stream;
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    recordAudio = URL.createObjectURL(e.data);
+    audio.srcObject = null;
+    audio.src = recordAudio;
+    audio.play();
+    playPauseBtn.addEventListener("click", handlePlayPause);
+    audio.addEventListener("play", handleLoad);
+    audio.loop = true;
+    playPauseBtn.style.color = "#000";
+    playPauseBtn.className = "fas fa-pause";
+  };
+  recorder.start();
+  recordBtn.addEventListener("click", handleDownload);
+  setTimeout(() => {
     recorder.stop();
+    recordBtn.disabled = false;
+  }, 5000);
+};
+const handlePlayPause = () => {
+  if (audio.paused) {
+    audio.play();
+    playPauseBtn.className = "fas fa-pause";
+  } else {
+    audio.pause();
+    playPauseBtn.className = "fas fa-play";
+  }
+};
+const handleSound = () => {
+  if (audio.muted) {
+    audio.muted = false;
+    volumeRange.value = volumeValue;
+    volume.className = "fas fa-volume-up";
+  } else {
+    audio.muted = true;
+    volumeRange.value = 0;
+    volume.className = "fas fa-volume-mute";
+  }
 };
 
-const startRecording = () => {
-    startBtn.innerText = "Stop Recording";
-    startBtn.removeEventListener("click", startRecording);
-    startBtn.addEventListener("click", stopRecording);
-    
-    recorder = new MediaRecorder(stream);
-    recorder.ondateavailable = (event) => {
-        audioFile = URL.createObjectURL(event.data);
-        audio.srcObject = null;
-        audio.play;
-    }
-    recorder.start();
+const handleVolume = (event) => {
+  const {
+    target: { value }
+  } = event;
+  if (audio.muted) {
+    audio.muted = false;
+    volume.className = "fas fa-volume-mute";
+  }
+  if (value === "0") {
+    volume.className = "fas fa-volume-off";
+  } else {
+    volume.className = "fas fa-volume-up";
+  }
+  audio.volume = volumeValue = value;
 };
 
-const init = async () => {
-    stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: false,
-    });
-    audio.srcObject = stream; // srcObject라는 속성은 없는 거 아냐?
-    audio.play; // vs. video.play() ?
+const handletimeupdate = () => {
+  currenTime.innerText = `0:0${Math.floor(audio.currentTime)}`;
+  timeline.value = Math.floor(audio.currentTime);
+};
+const handleLoad = () => {
+  totalTime.innerText = "0:05";
+  timeline.max = 5;
+};
+const handleTimeline = (e) => {
+  const {
+    target: { value }
+  } = e;
+  audio.currentTime = value;
 };
 
-init();
+recordBtn.addEventListener("click", handleStart);
 
-startBtn.addEventListener('click', startRecording);
+audio.addEventListener("timeupdate", handletimeupdate);
+volume.addEventListener("click", handleSound);
+volumeRange.addEventListener("input", handleVolume);
+timeline.addEventListener("input", handleTimeline);
