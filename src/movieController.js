70c6e9b19f607c05b9278ee2b2c6588abea059d0
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-20 16:22:44 +0900

    [#5] Solve assignment (8) where add movieDetail()

--- src/movieController.js
+++ src/movieController.js
@@ -1,7 +1,7 @@
 import {
   getMovieById,
   getMovies,
-  getMovieByMinimumRating,
+  // getMovieByMinimumRating,
   getMovieByMinimumYear
 } from "./db";
 
@@ -11,13 +11,22 @@ export const home = (req, res) => {
   return res.render("home", { movies });
 };
 
-export const movieDetail = (req, res) => {
+export const movieDetail = async (req, res) => {
   // home.pug에서 얻은 ID를 /:id에 보내면 movieDetail() 실행하여,
   // getMoviesById로 해당 영화를 가져와서, detail.pug에 보내주기
   const { params: { id } } = req;
-  const movie = getMovieById(id);
-  console.log(movie); // 왜 summary 같은 거 말고 quality, date_uploaded 같은 부가 정보만 오지?
-  return res.render("detail", { movie });
+  try {
+    const movie = await getMovieById(id);
+    // console.log(movie.title); // 왜 summary 같은 거 말고 quality, date_uploaded 같은 부가 정보만 오지? -> summary 같은 필요한 정보 잘 옴
+    const title = movie.title;
+    const year = movie.year;
+    const rating = movie.rating;
+    const runtime = movie.runtime;
+    const genres = movie.genres;
+    const summary = movie.summary;
+    return res.render("detail", { title, year, rating, runtime, genres, summary }); } catch (err) { // pug 파일에는 title 잘만 출력되는데, 왜 이런 에러가? TypeError: Cannot read property 'title' of undefined
+      console.log(err);
+    };
 };
 
 export const filterMovie = (req, res) => {
