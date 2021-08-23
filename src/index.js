f72654652d29ed27f0993e6f7784666b5163267b
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-23 21:17:01 +0900

    [#9] Practice assignment (13) where delete middleware.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 import routeRouter from "./routeRouter";
-import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const PORT = 4040;
@@ -12,7 +11,6 @@ const PORT = 4040;
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(localsMiddleware);
 
 app.use("/", routeRouter);
 
