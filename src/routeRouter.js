e67c7e9f1a500012bd8d57a20ecd7ec9d9f8fb10
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-23 20:30:24 +0900

    [#8] Practice Assignment (11) where not work regular expression when routing

--- src/routeRouter.js
+++ src/routeRouter.js
@@ -10,7 +10,7 @@ import {
 const routeRouter = express.Router();
 
 routeRouter.get("/", seeAllMovies);
-routeRouter.get("/movies/:id", movieDetail);
+routeRouter.get("/movies/:id(/61235/)", movieDetail);
 // routeRouter.route("/movies/:id/edit").get(getEdit).post(postEdit);
 // roiteRouter.route("/movies/:id/delete").get(movieDelete);
 
