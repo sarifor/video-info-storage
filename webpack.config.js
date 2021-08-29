0db13235185c988a9ed1da335853fc1081e8f6dc
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-08-29 16:19:27 +0900

    [#21] Solve assignment (21) use webpack to build scss (working)

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',    
     entry: {
-      test: './src/client/test.js',
-      test2: './src/client/test2.js',
+      test: ['./src/client/test.js', './src/client/test.scss'],
+      test2: ['./src/client/test2.js', './src/client/test2.scss'],
     },
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, 'assets'),
     },
-    modules: {
+    module: {
       rules: [
           {
               test: /\.scss$/,
-              use: ['style-loader', 'css-loader', 'sass-loader'],    
+              use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],    
           }
       ]
-    }    
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    ],      
   };
